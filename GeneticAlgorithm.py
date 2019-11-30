0edc3cfd1b58b84a806aed86ee1d4eed9ce68460
samuelchardy <37079171+samuelchardy@users.noreply.github.com>
2019-11-30 22:43:34 +0000

    #5 - Initial crossover implementation.

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -1,39 +1,72 @@
 import random
 import Game as myModule
 
-POPULATION = 10
-GENERATIONS = 10
-NUM_OF_CHROMOSOMES = 10
+class GeneticAlgorithm:
 
-chromosomes = [[0]*NUM_OF_CHROMOSOMES]*POPULATION
-fitness = [0]*NUM_OF_CHROMOSOMES
+    POPULATION = 10
+    GENERATIONS = 1
+    NUM_OF_CHROMOSOMES = 10
 
-game = myModule.Game()
+    tempLine = []
 
-#Generate Population Chromosomes
-for y in range(POPULATION):
-    for i in range(10):
-        chromosomes[y][i] = random.randint(0,3)
-    fitness[y] = game.runGame(chromosomes[y])
-    print(chromosomes[y], "  fitness: ", fitness[y])
+    chromosomes = [[0]*NUM_OF_CHROMOSOMES]*POPULATION
+    parChromosome = [[0]*NUM_OF_CHROMOSOMES]*2
+    fitness = [0]*NUM_OF_CHROMOSOMES
+
+    game = myModule.Game()
 
 
-for g in range(GENERATIONS):
-
-    #selection
-
-
-    #crossover
+    def __init__(self):
+        self.generatePopulation()
 
 
-    #mutation
+    def generatePopulation(self):
+        for y in range(0,self.POPULATION):
+            for i in range(0,self.NUM_OF_CHROMOSOMES):
+                self.chromosomes[y][i] = random.randint(0,3)
+            self.fitness[y] = self.game.runGame(self.chromosomes[y])
+            print(self.chromosomes[y], "  fitness: ", self.fitness[y], "   y: ", y)
+        print()
+        for a in range(10):
+            print(self.chromosomes[a] , "   y: ", a)
+        self.start(self.chromosomes)
 
 
-    #Fill new population
+    def start(self, chromosomes):
+        for g in range(self.GENERATIONS):
+            newChromosomes = [[0]*self.NUM_OF_CHROMOSOMES]*self.POPULATION
+
+            for c in range(0,self.POPULATION-1,2):
+                #selection             
+                self.parChromosome[0] = chromosomes[random.randint(0,self.POPULATION-1)]
+                self.parChromosome[1] = chromosomes[random.randint(0,self.POPULATION-1)]
+
+                newChromosomes[c] = self.parChromosome[1]
+                newChromosomes[c+1] = self.parChromosome[0]
+
+                print()
+                print(newChromosomes[c])
+                print(newChromosomes[c+1])
+                print()
+
+                #crossover
+                for f in range(0,1):
+                    if(random.random() < 0.8):
+                        crossPoint = random.randint(0,self.NUM_OF_CHROMOSOMES-1)
+                        for i in range(0,self.NUM_OF_CHROMOSOMES-1):
+                            if(i > crossPoint):
+                                newChromosomes[c+f][i] = self.parChromosome[f][i]
+
+                #mutation
+            print()
+            for c in range(self.POPULATION):
+                print(newChromosomes[c])
+
+            #Fill new population
+
+
+ga = GeneticAlgorithm()
 
 
 
-    
-    
-    
-    
+
